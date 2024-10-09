825a1bbf0af9ce4f53bb9aaccbe0dfc69f84e907
pjastr <24464854+pjastr@users.noreply.github.com>
2024-10-09 23:09:58 +0200

    fix

---
+++ lab2/CameraController.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset;
+
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+   void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ lab2/PlayerController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    private float movementX;
+    private float movementY;
+    private bool isJumping = false;
+    public float speed = 5;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+    void OnJump(InputValue movementValue)
+    {
+        rb.AddForce(Vector3.up * 5, ForceMode.Impulse);
+
+    }
+    private void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+        rb.AddForce(movement * speed);
+    }
+}
